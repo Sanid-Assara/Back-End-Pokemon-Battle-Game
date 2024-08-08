f37b452bccbba93acc5f4700fc84d5fd0087ffe3
Mohamed Moghazi <eng.moghazy@gmail.com>
2024-08-08 15:58:30 +0200

    fix

--- data/leaderBoardData.js
+++ data/leaderBoardData.js
@@ -17,7 +17,31 @@ const leaderBoardData = [
     },
     {
         "username": "player5",
+        "score": 140
+    },
+    {
+        "username": "moahaamad",
+        "score": 6400
+    },
+    {
+        "username": "Sanad",
+        "score": 120
+    },
+    {
+        "username": "Ahmed",
         "score": 1400
+    },
+    {
+        "username": "player6",
+        "score": 1600
+    },
+    {
+        "username": "player7",
+        "score": 1800
+    },
+    {
+        "username": "player8",
+        "score": 1900
     }
 ]
 
